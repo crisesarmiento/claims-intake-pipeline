fae1c30ed70f6d5751356dbe074605233d4084df
Cristian Sarmiento <85705213+crisesarmiento@users.noreply.github.com>
2026-04-20 05:34:44 -0300

    chore: bootstrap CDK project with LocalStack config

--- claims-instake-pipeline/app.py
+++ claims-instake-pipeline/app.py
@@ -3,8 +3,14 @@ import os
 import aws_cdk as cdk
 from claims_instake_pipeline.claims_instake_pipeline_stack import ClaimsInstakePipelineStack
 
+AWS_ENDPOINT = os.getenv("AWS_ENDPOINT")
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+CDK_DEFAULT_ACCOUNT = os.getenv("CDK_DEFAULT_ACCOUNT")
 
 app = cdk.App()
-ClaimsInstakePipelineStack(app, "ClaimsInstakePipelineStack")
+ClaimsInstakePipelineStack(app, "ClaimsInstakePipelineStack", env=cdk.Environment(
+    account=CDK_DEFAULT_ACCOUNT,
+    region=AWS_REGION
+))
 
 app.synth()
